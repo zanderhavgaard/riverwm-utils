cf1db6c1b970f964614c4a0b0b173e69d2a48732
zanderhavgaard <zander@havgaard.dk>
2025-04-20 17:13:01 +0200

    run black formatter on riverwm_utils.py

--- riverwm_utils/riverwm_utils.py
+++ riverwm_utils/riverwm_utils.py
@@ -1,27 +1,36 @@
-'''Utilities for river wayland compositor'''
+"""Utilities for river wayland compositor"""
+
 import sys
 import os
 import argparse
 import struct
+
 # pylint: disable=global-statement
 try:
     from pywayland.protocol.wayland import WlOutput
     from pywayland.protocol.wayland import WlSeat
     from pywayland.protocol.wayland import WlRegistry
     from pywayland.protocol.river_control_unstable_v1 import ZriverControlV1
-    from pywayland.protocol.river_status_unstable_v1 import ZriverStatusManagerV1  # noqa: E501
+    from pywayland.protocol.river_status_unstable_v1 import (
+        ZriverStatusManagerV1,
+    )  # noqa: E501
 except ModuleNotFoundError:
     try:
         from pywayland.scanner.protocol import Protocol
         import pywayland
+
         this_dir = os.path.split(__file__)[0]
         protocol_dir = os.path.join(this_dir)
-        input_files = ['wayland.xml',
-                       'river-control-unstable-v1.xml',
-                       'river-status-unstable-v1.xml']
+        input_files = [
+            "wayland.xml",
+            "river-control-unstable-v1.xml",
+            "river-status-unstable-v1.xml",
+        ]
 
-        protocols = [Protocol.parse_file(os.path.join(
-            protocol_dir, input_file)) for input_file in input_files]
+        protocols = [
+            Protocol.parse_file(os.path.join(protocol_dir, input_file))
+            for input_file in input_files
+        ]
         protocol_imports = {
             interface.name: protocol.name
             for protocol in protocols
@@ -29,26 +38,27 @@ except ModuleNotFoundError:
         }
 
         pywayland_dir = os.path.split(pywayland.__file__)[0]
-        output_dir = os.path.join(pywayland_dir, 'protocol')
+        output_dir = os.path.join(pywayland_dir, "protocol")
         for protocol in protocols:
             protocol.output(output_dir, protocol_imports)
-        print('Generated river bindings.')
-        print('Please try running cycle-focused-tags again.')
+        print("Generated river bindings.")
+        print("Please try running cycle-focused-tags again.")
 
     except ImportError:
         THIS_DIR = os.path.split(__file__)[0]
-        PROTOCOL_DIR = os.path.normpath(
-            os.path.join(THIS_DIR, '..', 'protocol'))
-        ERROR_TEXT = (f'''
+        PROTOCOL_DIR = os.path.normpath(os.path.join(THIS_DIR, "..", "protocol"))
+        ERROR_TEXT = (
+            f"""
         Your pywayland package does not have bindings for
         river-control-unstable-v1 and/or river-status-unstable-v1.
 
         An attempt was made to generate them but it failed. You may be able to
         generate the manually with the following command:
 
-        python3 -m pywayland.scanner -i {PROTOCOL_DIR}/wayland.xml '''
-                      f'{PROTOCOL_DIR}/river-control-unstable-v1.xml '
-                      f'{PROTOCOL_DIR}/river-status-unstable-v1.xml')
+        python3 -m pywayland.scanner -i {PROTOCOL_DIR}/wayland.xml """
+            f"{PROTOCOL_DIR}/river-control-unstable-v1.xml "
+            f"{PROTOCOL_DIR}/river-status-unstable-v1.xml"
+        )
 
         print(ERROR_TEXT)
 
@@ -65,7 +75,8 @@ SEAT = None
 
 
 class Output:
-    '''Represents a wayland output a.k.a. a display'''
+    """Represents a wayland output a.k.a. a display"""
+
     def __init__(self):
         self.wl_output = None
         self.focused_tags = None
@@ -74,37 +85,38 @@ class Output:
         self.status = None
 
     def destroy(self) -> None:
-        '''Cleanup'''
+        """Cleanup"""
         if self.wl_output is not None:
             self.wl_output.destroy()
         if self.status is not None:
             self.status.destroy()
 
     def configure(self) -> None:
-        '''Setup'''
+        """Setup"""
         self.status = STATUS_MANAGER.get_river_output_status(self.wl_output)
         self.status.user_data = self
         self.status.dispatcher["focused_tags"] = self.handle_focused_tags
         self.status.dispatcher["view_tags"] = self.handle_view_tags
 
     def handle_focused_tags(self, _, tags: int) -> None:
-        '''Handle Event'''
+        """Handle Event"""
         self.focused_tags = tags
 
     def handle_view_tags(self, _, tags: int) -> None:
-        '''Handle Event'''
+        """Handle Event"""
         self.view_tags = tags
 
 
 class Seat:
-    '''Represents a wayland seat'''
+    """Represents a wayland seat"""
+
     def __init__(self):
         self.wl_seat = None
         self.status = None
         self.focused_output = None
 
     def destroy(self) -> None:
-        '''Cleanup'''
+        """Cleanup"""
         if self.wl_seat is not None:
             self.wl_seat.destroy()
 
@@ -112,34 +124,35 @@ class Seat:
             self.status.destroy()
 
     def configure(self) -> None:
-        '''Setup'''
+        """Setup"""
         self.status = STATUS_MANAGER.get_river_seat_status(self.wl_seat)
         self.status.user_data = self
         self.status.dispatcher["focused_output"] = self.handle_focused_output
 
     def handle_focused_output(self, _, wl_output: WlOutput) -> None:
-        '''Handle Event'''
+        """Handle Event"""
         for output in OUTPUTS:
             if output.wl_output == wl_output:
                 self.focused_output = output
 
 
-def registry_handle_global(registry: WlRegistry, wid: int, interface: str,
-                           version: int) -> None:
-    '''Main Event Handler'''
+def registry_handle_global(
+    registry: WlRegistry, wid: int, interface: str, version: int
+) -> None:
+    """Main Event Handler"""
     global STATUS_MANAGER
     global CONTROL
     global SEAT
 
-    if interface == 'zriver_status_manager_v1':
+    if interface == "zriver_status_manager_v1":
         STATUS_MANAGER = registry.bind(wid, ZriverStatusManagerV1, version)
-    elif interface == 'zriver_control_v1':
+    elif interface == "zriver_control_v1":
         CONTROL = registry.bind(wid, ZriverControlV1, version)
-    elif interface == 'wl_output':
+    elif interface == "wl_output":
         output = Output()
         output.wl_output = registry.bind(wid, WlOutput, version)
         OUTPUTS.append(output)
-    elif interface == 'wl_seat':
+    elif interface == "wl_seat":
         # We only care about the first seat
         if SEAT is None:
             SEAT = Seat()
@@ -147,7 +160,7 @@ def registry_handle_global(registry: WlRegistry, wid: int, interface: str,
 
 
 def prepare_display(display: Display) -> None:
-    '''Prepare display global objects'''
+    """Prepare display global objects"""
     display.connect()
 
     registry = display.get_registry()
@@ -177,7 +190,7 @@ def prepare_display(display: Display) -> None:
 
 
 def close_display(display: Display) -> None:
-    '''Clean up objects'''
+    """Clean up objects"""
     SEAT.destroy()
     for output in OUTPUTS:
         output.destroy()
@@ -192,8 +205,8 @@ def close_display(display: Display) -> None:
 
 
 def check_n_tags(n_tags: int) -> int:
-    '''Check max tag number argument'''
-    error_string = f'Invalid max number of tags: {n_tags}'
+    """Check max tag number argument"""
+    error_string = f"Invalid max number of tags: {n_tags}"
     try:
         i_n_tags = int(n_tags)
     except Exception as exc:
@@ -206,51 +219,61 @@ def check_n_tags(n_tags: int) -> int:
 
 
 def parse_command_line() -> argparse.Namespace:
-    '''Read commandline arguments'''
+    """Read commandline arguments"""
     parser = argparse.ArgumentParser(
-        description='Change to either the next or previous tags.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description="Change to either the next or previous tags.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        'n_cycle', default=1, nargs='?', type=int,
-        help=('Number of tags to cycle through. Signed integer.')
+        "n_cycle",
+        default=1,
+        nargs="?",
+        type=int,
+        help=("Number of tags to cycle through. Signed integer."),
     )
     parser.add_argument(
-        'n_tags', default=32, nargs='?', type=check_n_tags,
-        help=('The tag number the cycling should loop back to the first tag '
-              'or to the last tag from the first tag. Integer between 1 and '
-              '32 inclusive.')
+        "n_tags",
+        default=32,
+        nargs="?",
+        type=check_n_tags,
+        help=(
+            "The tag number the cycling should loop back to the first tag "
+            "or to the last tag from the first tag. Integer between 1 and "
+            "32 inclusive."
+        ),
     )
     parser.add_argument(
-        '--all-outputs', '-a', dest='all_outputs', action='store_true',
-        help='Cycle the tags for all outputs (following the active output).'
+        "--all-outputs",
+        "-a",
+        dest="all_outputs",
+        action="store_true",
+        help="Cycle the tags for all outputs (following the active output).",
     )
     parser.add_argument(
-        '--follow', '-f', dest='follow', action='store_true',
-        help='Move the active window when cycling.'
+        "--follow",
+        "-f",
+        dest="follow",
+        action="store_true",
+        help="Move the active window when cycling.",
     )
     parser.add_argument(
-        '--skip-occupied', '-o', action='store_true',
-        help='Skip occupied tags.'
+        "--skip-occupied", "-o", action="store_true", help="Skip occupied tags."
     )
     parser.add_argument(
-        '--skip-empty', '-s', action='store_true',
-        help='Skip empty tags.'
+        "--skip-empty", "-s", action="store_true", help="Skip empty tags."
     )
     parser.add_argument(
-        '--debug', '-d', action='store_true',
-        help='Enable debugging output.'
+        "--debug", "-d", action="store_true", help="Enable debugging output."
     )
     return parser.parse_args()
 
 
 def get_occupied_tags(cli_args: argparse.Namespace) -> int:
-    '''Return bitmap of occupied tags as int'''
+    """Return bitmap of occupied tags as int"""
     used_tags = (1 << cli_args.n_tags) - 1
 
     if not cli_args.all_outputs or len(OUTPUTS) == 1:
-        return get_occupied_from_view_tags(
-            SEAT.focused_output.view_tags) & used_tags
+        return get_occupied_from_view_tags(SEAT.focused_output.view_tags) & used_tags
 
     occupied_tags = 0
     for output in OUTPUTS:
@@ -260,27 +283,29 @@ def get_occupied_tags(cli_args: argparse.Namespace) -> int:
 
 
 def get_occupied_from_view_tags(view_tags: int) -> int:
-    '''Return bitmap of view_tags occupied tags as int'''
+    """Return bitmap of view_tags occupied tags as int"""
     occupied_tags = 0
     nviews = int(len(view_tags) / 4)
-    for view in struct.unpack(f'{nviews}I', view_tags):
+    for view in struct.unpack(f"{nviews}I", view_tags):
         occupied_tags |= view
 
     return occupied_tags
 
 
-def get_new_tags(cli_args: argparse.Namespace,
-                 occupied_tags: int) -> int:
-    '''Return the new tag set'''
+def get_new_tags(cli_args: argparse.Namespace, occupied_tags: int) -> int:
+    """Return the new tag set"""
     used_tags = (1 << cli_args.n_tags) - 1
     tags = SEAT.focused_output.focused_tags & used_tags
 
-    if (cli_args.n_cycle == 0  # noqa: W504
-        or cli_args.skip_empty and occupied_tags == 0
+    if (
+        cli_args.n_cycle == 0  # noqa: W504
+        or cli_args.skip_empty
+        and occupied_tags == 0
         # All tags empty & we want to skip empty tags
-        or cli_args.skip_occupied and used_tags == (used_tags ^ occupied_tags)
+        or cli_args.skip_occupied
+        and used_tags == (used_tags ^ occupied_tags)
         # All tags occupied & we want to skip occupied tags
-            ):
+    ):
         return tags
 
     i = 0
@@ -294,7 +319,7 @@ def get_new_tags(cli_args: argparse.Namespace,
                 tags ^= last_tag
                 new_tags = 1
 
-            new_tags |= (tags << 1)
+            new_tags |= tags << 1
 
         else:
             # If lowest bit is set (first tag) => unset it and set
@@ -303,7 +328,7 @@ def get_new_tags(cli_args: argparse.Namespace,
                 tags ^= 1
                 new_tags = last_tag
 
-            new_tags |= (tags >> 1)
+            new_tags |= tags >> 1
 
         tags = new_tags
 
@@ -321,19 +346,18 @@ def get_new_tags(cli_args: argparse.Namespace,
     # Looped over all tags without returning, either skip options caused
     # none of the potential tags to be viable or something went wrong.
     if cli_args.skip_empty:
-        print('Cycle failed: all tags empty')
+        print("Cycle failed: all tags empty")
     elif cli_args.skip_occupied:
-        print('Cycle failed: all tags occupied')
+        print("Cycle failed: all tags occupied")
     else:
         # Something is wrong, bail out.
-        print('Cycle failed: looped over all tags')
+        print("Cycle failed: looped over all tags")
 
     return initial_tags
 
 
-
 def set_new_tags(cli_args: argparse.Namespace, new_tags: int) -> None:
-    '''Set the focused tags'''
+    """Set the focused tags"""
     if cli_args.follow:
         CONTROL.add_argument("set-view-tags")
         CONTROL.add_argument(str(new_tags))
@@ -368,7 +392,7 @@ def set_new_tags(cli_args: argparse.Namespace, new_tags: int) -> None:
 
 
 def cycle_focused_tags() -> None:
-    '''Shift to next or previous tags'''
+    """Shift to next or previous tags"""
     args = parse_command_line()
     display = Display()
     prepare_display(display)
@@ -377,9 +401,9 @@ def cycle_focused_tags() -> None:
     new_tags = get_new_tags(args, occupied_tags)
 
     if args.debug:
-        print(f'cur 0b{SEAT.focused_output.focused_tags:032b}')
-        print(f'occ 0b{occupied_tags:032b}')
-        print(f'new 0b{new_tags:032b}')
+        print(f"cur 0b{SEAT.focused_output.focused_tags:032b}")
+        print(f"occ 0b{occupied_tags:032b}")
+        print(f"new 0b{new_tags:032b}")
 
     set_new_tags(args, new_tags)
 
